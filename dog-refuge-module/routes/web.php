3295fd0733d03eb6c83c2f6cf2af6d4f8468a6d4
zacdagostino <22979843@student.uwa.edu.au>
2024-10-17 21:56:02 +0800

    #Register resource routes for CarerController in web.php for managing carers

--- dog-refuge-module/routes/web.php
+++ dog-refuge-module/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Register resource routes for the CarerController
+Route::resource('carers', CarerController::class);
